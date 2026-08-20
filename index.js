21ce1b56e6a57b2dc42ed180d8a43df41358846b
arulzxd <arulzz.xd@gmail.com>
2026-08-21 03:10:36 +0700

    Fix capitalization of 'Const' to 'const'

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Const express = require('express');
+const express = require('express');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const mongoose = require('mongoose');
